d139b1d4985843e8c7dc7ada433c282decb53a8a
HP <farishtasulaiman@gmail.com>
2024-10-12 23:42:16 +0200

    skapat game mappen

--- Hemuppgift_Arv_Temp/TakePins.cs
+++ Hemuppgift_Arv_Temp/TakePins.cs
@@ -2,7 +2,7 @@
 {
     internal class TakePins
     {
-        //Här är main klassen där koden ska testas, lägg inmappen
+        //Här är main klassen där koden ska testas, lägg in mappen
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!"); 
