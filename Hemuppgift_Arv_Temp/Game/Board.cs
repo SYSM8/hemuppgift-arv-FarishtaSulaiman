9f65ba201ce51000f05be57258593c242e4bfe8f
HP <farishtasulaiman@gmail.com>
2024-10-13 21:57:56 +0200

    lagt till metod för getNoPins, takePins med villkor

--- Hemuppgift_Arv_Temp/Game/Board.cs
+++ Hemuppgift_Arv_Temp/Game/Board.cs
@@ -17,6 +17,28 @@ namespace Hemuppgift_Arv_Temp.Game
         {
             noPins = pins;
         }
-        
+        // publik metod och returnerar antal stickor på bord, 
+        // pga noPins är privat måste denna metod användas
+        // så den kan läsas av utanför klassen
+        public int getNoPins()
+        {
+            return noPins;
+        }
+
+        // metod som villkorar dragen i spelet, 
+        // skickar felmeddelande om man tar fel antal stickor eller om det inte finns tillräckligt många stickor kvar
+        public void takePins(int pins)
+        {
+            if (pins < 1 || pins > 2)
+            {
+                throw new ArgumentException("Du kan bara ta 1 eller 2 stickor.");
+            }
+            if (noPins < pins)
+            {
+                throw new ArgumentException("Det finns inte tillräckligt med stickor kvar");
+            }
+
+            noPins -= pins;
+        }
     }
 }
